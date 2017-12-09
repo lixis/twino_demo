1ef186b995f9eda059bd1010d3e6e49f88cb9c5c
VasPE <p.vasilyuk@bssys.com>
2017-12-09 18:19:47 +0300

    fix

--- src/test/java/com/twino/demo/contoller/TwinoContollerTest.java
+++ src/test/java/com/twino/demo/contoller/TwinoContollerTest.java
@@ -1,29 +1,31 @@
 package com.twino.demo.contoller;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
-@ContextConfiguration(classes = TwinoContollerTest.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class TwinoContollerTest {
 
-//    @Autowired
-//    private TestRestTemplate restTemplate;
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
 
     @Test
     public void getLoanById_Success() {
-        String body = restTemplate.getForObject("/test", String.class);
 
-        Assertions.assertThat(body).isEqualTo("new_loan");
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/test",
+                String.class)).contains("new_loan");
     }
 }
